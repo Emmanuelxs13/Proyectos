475b86df3f7b52a3e5a17dd411ae3e8ed2e0a979
Emmanuel Berrio <emmanuelberriojimenez@gmail.com>
2023-05-30 12:33:41 -0500

    Se sube la actualización de la app con nuevos estilos y funcionalidades

--- Apps/App_Peliculas/src/pages/MovieDetails.jsx
+++ Apps/App_Peliculas/src/pages/MovieDetails.jsx
@@ -1,3 +1,33 @@
-export function MovieDetails(){
-    return(<h1>Movie Details</h1>)
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { get } from "../data/httpClient"
+import { getMovieImg } from "../utils/getMovieImg"
+import "../pages/MovieDetails.css"
+
+export function MovieDetails() {
+    const { movieId } = useParams();
+    const [movie, setMovie] = useState([]);
+    const [generos, setGeneros] = useState([]);
+
+
+    // Cada vez que se cambie el id se ejecute esta consulta
+    useEffect(() => {
+        get("/movie/" + movieId).then((data) => {
+            setMovie(data);
+            setGeneros(data.genres[0]);
+        });
+    }, [movieId]);
+    const imageUrl = getMovieImg(movie.poster_path, 500);
+
+    return (
+        <div className="detailsContainer">
+            <img src={imageUrl} alt={movie.title} className="col movieImage" />
+            <div className="col movieDetails">
+                <p className="title">{movie.title}</p>
+                <br />
+                <p><strong>Genero: </strong>{generos.name}</p>
+                <br />
+                <p><strong>Descripción: </strong>{movie.overview}</p>
+            </div>
+        </div>)
 }
